5d4a7debaeec2a497612dc7de9f0e903bff4c40a
jamie-ralph <jamesryanralph@outlook.com>
2021-06-25 09:45:11 +0100

    changed test output to single csv log file

--- R/test-get-data.R
+++ R/test-get-data.R
@@ -4,21 +4,39 @@ library(statswalesr)
 
 df <- statswales_get_dataset("WIMD1909")
 
-metrics <- tryCatch(
+# Latest results
+new_metrics <- tryCatch(
     {
         data.frame(
         dataset = "WIMD1909",
         rows = nrow(df),
-        cols = ncol(df)
+        cols = ncol(df),
+        date_time = make.names(Sys.time())
             )
     },
     error = function(cnd) {
         data.frame(
             dataset = "WIMD1909",
             rows = "NULL",
-            cols = "NULL"
+            cols = "NULL",
+            date_time = make.names(Sys.time())
         )
     }
 )
 
-write.csv(metrics, file = paste0("results/metrics-", make.names(Sys.time()), ".csv"))
+# Read in previous results if present
+if("metrics-results.csv" %in% list.files("results")) {
+
+    old_metrics <- read.csv("results/metrics-results.csv")
+
+    updated_metrics <- rbind(old_metrics, new_metrics)
+
+} else {
+
+    updated_metrics <- new_metrics
+
+}
+
+
+# Write out to new csv
+write.csv(updated_metrics, file = "results/metrics-results.csv")
